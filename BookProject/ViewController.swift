007001c0960cf9bcf6d3ebb480b8fe22a7aed3f5
MakGris <maxgrishenko@gmail.com>
2024-03-15 18:38:57 +0700

    Initial Commit

---
+++ BookProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookProject
+//
+//  Created by Maksim Grischenko on 15.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
